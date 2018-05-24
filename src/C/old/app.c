90da31928f00ff1d4e56a475068263fd7b147bbd
DavidRCastello <davidrodri.c@gmail.com>
2018-05-24 11:49:20 +0200

    first

---
+++ src/C/old/app.c
@@ -0,0 +1,358 @@
+#include <includes.h>
+#include "xadcps.h"
+#include "xstatus.h"
+#include "stdio.h"
+#include "xparameters.h"
+#include "xgpio.h"
+#ifdef XPAR_INTC_0_DEVICE_ID
+ #include "xintc.h"
+#else
+ #include "xscugic.h"
+#endif
+
+
+/*
+*********************************************************************************************************
+*                                             LOCAL DEFINES
+*********************************************************************************************************
+*/
+
+#define INTR_DEVICE_ID      XPAR_PS7_SCUGIC_0_DEVICE_ID
+#define INTL_DEVICE_ID      XPAR_PS7_SCUGIC_1_DEVICE_ID
+#define INTC_GPIO_INTERRUPT_ID XPAR_FABRIC_AXI_GPIO_0_IP2INTC_IRPT_INTR
+#define XADC_DEVICE_ID 		XPAR_XADCPS_0_DEVICE_ID
+
+#define BUTTON_CHANNEL		1 // Input channel
+
+/*
+*********************************************************************************************************
+*                                            LOCAL VARIABLES
+*********************************************************************************************************
+*/
+
+static XAdcPs XAdcInst;      /* XADC driver instance */
+XAdcPs *XAdcInstPtr = &XAdcInst;
+XScuGic INTCInst;
+static XGpio butr, butl; /* The Instance of the GPIO Driver */
+int *axi_pointer = (int *) XPAR_VGA_CONTROL_0_S00_AXI_BASEADDR;
+int InterruptFlag; /* Flag used to indicate that an interrupt has occurred */
+
+
+
+/*
+*********************************************************************************************************
+*                                         FUNCTION PROTOTYPES
+*********************************************************************************************************
+*/
+
+static void INT_Intr_Handler(void *baseaddr_p);
+static void INT_Intl_Handler(void *baseaddr_p);
+static int InterruptRSystemSetup(XScuGic *XScuGicInstancePtr);
+static int InterruptLSystemSetup(XScuGic *XScuGicInstancePtr);
+static int IntlInitFunction(u16 DeviceId, XGpio *GpioInstancePtr);
+static int IntrInitFunction(u16 DeviceId, XGpio *GpioInstancePtr);
+static void Peripheral_Init		(void);
+
+
+//----------------------------------------------------
+// INITIAL SETUP FUNCTIONS
+//----------------------------------------------------
+
+void INT_Intr_Handler(void *InstancePtr) {
+    // Disable GPIO interrupts
+    XGpio_InterruptDisable(&GPIOInst, INTR_INT); //SWITCH or Pulsador
+    // Ignore additional button presses
+    if ((XGpio_InterruptGetStatus(&GPIOInst) & INTR_INT) != INTR_INT) {
+        return;
+    }
+
+    /* Sets the interrupt flag */
+    InterruptFlag = 1;
+
+    (void) XGpio_InterruptClear(&GPIOInst, INTR_INT);
+    // Enable GPIO interrupts
+    XGpio_InterruptEnable(&GPIOInst, INTR_INT);
+}
+
+void INT_Intl_Handler(void *InstancePtr) {
+    // Disable GPIO interrupts
+    XGpio_InterruptDisable(&GPIOInst, INTL_INT); //SWITCH or Pulsador
+    // Ignore additional button presses
+    if ((XGpio_InterruptGetStatus(&GPIOInst) & INTL_INT) != INTL_INT) {
+        return;
+    }
+
+    /* Sets the interrupt flag */
+    InterruptFlag = 1;
+
+    (void) XGpio_InterruptClear(&GPIOInst, INTL_INT);
+    // Enable GPIO interrupts
+    XGpio_InterruptEnable(&GPIOInst, INTL_INT);
+}
+
+int InterruptRSystemSetup(XScuGic *XScuGicInstancePtr) {
+    // Enable interrupt
+    XGpio_InterruptEnable(&GPIOInst, INTR_INT);
+    XGpio_InterruptGlobalEnable(&GPIOInst);
+
+    Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT,
+            (Xil_ExceptionHandler) XScuGic_InterruptHandler,
+            XScuGicInstancePtr);
+    Xil_ExceptionEnable();
+
+    return XST_SUCCESS;
+}
+
+int InterruptLSystemSetup(XScuGic *XScuGicInstancePtr) {
+    // Enable interrupt
+    XGpio_InterruptEnable(&GPIOInst, INTL_INT);
+    XGpio_InterruptGlobalEnable(&GPIOInst);
+
+    Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT,
+            (Xil_ExceptionHandler) XScuGic_InterruptHandler,
+            XScuGicInstancePtr);
+    Xil_ExceptionEnable();
+
+    return XST_SUCCESS;
+}
+
+int IntlInitFunction(u16 DeviceId, XGpio *GpioInstancePtr) {
+    XScuGic_Config *IntlConfig;
+    int status;
+
+    // Interrupt controller initialisation
+    IntlConfig = XScuGic_LookupConfig(DeviceId);
+    status = XScuGic_CfgInitialize(&INTLInst, IntlConfig,
+            IntlConfig->CpuBaseAddress);
+    if (status != XST_SUCCESS)
+        return XST_FAILURE;
+
+    // Call to interrupt setup
+    status = InterruptLSystemSetup(&INTLInst);
+    if (status != XST_SUCCESS)
+        return XST_FAILURE;
+
+    // Connect GPIO interrupt to handler
+    status = XScuGic_Connect(&INTLInst, INTL_GPIO_INTERRUPT_ID,
+            (Xil_ExceptionHandler) INT_Intl_Handler, (void *) GpioInstancePtr);
+    if (status != XST_SUCCESS)
+        return XST_FAILURE;
+
+    // Enable GPIO interrupts interrupt
+    XGpio_InterruptEnable(GpioInstancePtr, 1);
+    XGpio_InterruptGlobalEnable(GpioInstancePtr);
+
+    // Enable GPIO and timer interrupts in the controller
+    XScuGic_Enable(&INTLInst, INTL_GPIO_INTERRUPT_ID);
+
+    return XST_SUCCESS;
+}
+
+int IntrInitFunction(u16 DeviceId, XGpio *GpioInstancePtr) {
+    XScuGic_Config *IntrConfig;
+    int status;
+
+    // Interrupt controller initialisation
+    IntrConfig = XScuGic_LookupConfig(DeviceId);
+    status = XScuGic_CfgInitialize(&INTRInst, IntrConfig,
+            IntrConfig->CpuBaseAddress);
+    if (status != XST_SUCCESS)
+        return XST_FAILURE;
+
+    // Call to interrupt setup
+    status = InterrupRtSystemSetup(&INTRInst);
+    if (status != XST_SUCCESS)
+        return XST_FAILURE;
+
+    // Connect GPIO interrupt to handler
+    status = XScuGic_Connect(&INTRInst, INTR_GPIO_INTERRUPT_ID,
+            (Xil_ExceptionHandler) INT_Intr_Handler, (void *) GpioInstancePtr);
+    if (status != XST_SUCCESS)
+        return XST_FAILURE;
+
+    // Enable GPIO interrupts interrupt
+    XGpio_InterruptEnable(GpioInstancePtr, 1);
+    XGpio_InterruptGlobalEnable(GpioInstancePtr);
+
+    // Enable GPIO and timer interrupts in the controller
+    XScuGic_Enable(&INTRInst, INTR_GPIO_INTERRUPT_ID);
+
+    return XST_SUCCESS;
+}
+
+void Peripheral_Init()
+{
+	int Status;
+	XAdcPs_Config *ConfigPtr;
+
+    /* Initialize the GPIO driver. If an error occurs then exit */
+    	Status = XGpio_Initialize(&Gpio, GPIO_DEVICE_ID);
+    	if (Status != XST_SUCCESS) {
+    		return XST_FAILURE;
+    	}
+
+    	/*
+    	 * Perform a self-test on the GPIO.  This is a minimal test and only
+    	 * verifies that there is not any bus error when reading the data
+    	 * register
+    	 */
+    	XGpio_SelfTest(&Gpio);
+
+    	/*
+    	 * Setup direction register so the switch is an input and the LED is
+    	 * an output of the GPIO
+    	 */
+    	XGpio_SetDataDirection(&Gpio, BUTTON_CHANNEL, 1);
+
+    	/*
+    	 * Initialize the XAdc driver.
+    	 */
+    	ConfigPtr = XAdcPs_LookupConfig(XADC_DEVICE_ID);
+
+
+    	XAdcPs_CfgInitialize(XAdcInstPtr, ConfigPtr,
+    				ConfigPtr->BaseAddress);
+
+    	/*
+    	 * Self Test the XADC/ADC device
+    	 */
+    	Status = XAdcPs_SelfTest(XAdcInstPtr);
+
+
+    	/*
+    	 * Disable the Channel Sequencer before configuring the Sequence
+    	 * registers.
+    	 */
+    	XAdcPs_SetSequencerMode(XAdcInstPtr, XADCPS_SEQ_MODE_SAFE);
+}
+
+int main()
+{
+    unsigned int temperature_raw;
+    unsigned int temperature_C;
+    unsigned int temperature, t_temperature;
+   // unsigned int tempRealToSend, tempThreshToSend;
+    unsigned int max_temp = 40;
+    int alarm = 0;
+    OS_ERR  err;
+    int over_under = 0;
+
+    int new_dataR;
+    int old_dataR;
+    int new_dataL;
+    int old_dataL;
+    
+    /* Variable initialisation */
+    InterruptFlag = 0;
+
+    init_platform();
+
+    // Initialise GPIO
+    
+   
+        
+    status = XGpio_Initialize(&butr, INTR_DEVICE_ID);
+    if (status != XST_SUCCESS)
+        return XST_FAILURE;
+
+    status = XGpio_Initialize(&butl, INTL_DEVICE_ID);
+    if (status != XST_SUCCESS)
+        return XST_FAILURE;
+    
+     XGpio_SetDataDirection(&butr, BUTTON_CHANNEL, 0xFF); 
+     XGpio_SetDataDirection(&butl, BUTTON_CHANNEL, 0xFF); 
+    
+    //ADC temperature initialization??
+     //ADC temp init
+    Peripheral_Init();
+    
+    //DirectionMask is a bitmask specifying which discretes are input and which are output. Bits set to 0 are output and bits set to 1 are input.
+    
+    
+    // Config GPIO channel 1 as input
+    //XGpio_SetDataDirection(&GPIOInst, SWITCH_CHANNEL, 0xFF); //El channel 2 estaba en 0x00
+
+    // Initialize interrupt controller
+    status = IntrInitFunction(INTR_DEVICE_ID, &butr);
+    if (status != XST_SUCCESS)
+        return XST_FAILURE;
+
+    // Initialize interrupt controller
+    status = IntlInitFunction(INTL_DEVICE_ID, &butl);
+    if (status != XST_SUCCESS)
+        return XST_FAILURE;
+
+    //*axi_pointer = 1; // Change the value, aqui deberia ir algo como "11 primeros bits temp_C, 11 siguientes bits max_temp, 1 bit over_under"
+
+    // Reads initial value of the Interruptor
+  /*  new_dataR = XGpio_DiscreteRead(&GPIOInst, BUTTONL_CHANNEL);
+    old_dataR = new_dataR;
+
+    new_dataL = XGpio_DiscreteRead(&GPIOInst, BUTTONR_CHANNEL);
+    old_dataL = new_dataL;*/
+   
+    while (1) {
+        // xadc_get_value_temp
+        //temperature_raw = XAdcPs_GetAdcData(XAdcInstPtr, BUTTON_CHANNEL);
+        temperature_raw = XAdcPs_GetAdcData(XAdcInstPtr, XADCPS_CH_TEMP);
+        temperature_C = (int) XAdcPs_RawToTemperature(temperature_raw);
+
+        if(temperature_C > 80){
+            alarm = 1;
+        }else if( temperature_C <80){
+            alarm = 0;
+        }
+        
+        /* Test for an interrupt produced by GPIO*/
+        if (InterruptFlag == 1)
+        {   
+            print("Interruption detected\r\n");
+            InterruptFlag = 0; // resets the interrupt flag
+            new_dataR = XGpio_DiscreteRead(&butr, BUTTON_CHANNEL);
+            new_dataL = XGpio_DiscreteRead(&butl, BUTTON_CHANNEL);
+
+            /* Rising edge detection*/
+            if ((new_dataR == 1) && (old_dataR == 0))
+            {
+                print("Right Button pressed\r\n");
+                if(max_temp < 80)
+                    max_temp ++;
+            }
+
+            if ((new_dataL == 1) && (old_dataL == 0))
+            {   
+                print("Left Button pressed\r\n");
+
+                if(max_temp > 0)
+                    max_temp --;
+            }
+            old_dataL = new_dataL;
+            old_dataR = new_dataR;
+
+            // temp*(1280/80) = temp*160
+            temperature = temperature_C * 160;
+            t_temperature = max_temp * 160;
+            print("temperature : %d\n / %d", temperature_C, temperature);
+            print("t_temperature : %d / %d\n", max_temp, t_temperature);
+        }
+
+        *axi_pointer = alarm;
+        *axi_pointer = temperature;
+        *axi_pointer = t_temperature;
+        
+        //Waits 100ms
+        OSTimeDlyHMSM(0, 0, 0, 100, OS_OPT_TIME_HMSM_STRICT, &err);                                     /* Waits for 100 milliseconds. 
+
+        /* Displays the value of the counter on LED7 and LED6*/
+        //XGpio_DiscreteWrite(&GPIOInst, LED_CHANNEL, counter);
+        /* Enables or disables the 28-bit counter depending on internal counter variable*/
+        /*if (counter == 0)
+            *count28_pointer = 1;
+        else
+            *count28_pointer = 0;
+            */
+    }
+
+    cleanup_platform();
+    return 0;
+}
---
+++ src/vga_control_commented.vhd
@@ -0,0 +1,208 @@
+library ieee;
+
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+
+entity vga_control is
+port(
+	data_out: in std_logic_vector(11 downto 0);
+	trigger_level: in std_logic_vector(8 downto 0);
+	clk:	in std_logic;
+	reset:	in std_logic;
+	gpio_in: in std_logic_vector(22 downto 0);
+	sw0: in std_logic;
+	vsync:	out std_logic;
+	hsync:	out std_logic;
+	red:	out std_logic_vector(3 downto 0);
+	green:	out std_logic_vector(3 downto 0);
+	blue:	out std_logic_vector(3 downto 0);
+	addr_out: out std_logic_vector(11 downto 0)
+	);
+end vga_control;
+
+
+architecture arch of vga_control is
+	constant PPL: integer:= 1280;	-- pixels per line
+	constant HFP: integer:= 48;		-- hsync front porch
+	constant HBP: integer:= 248;	-- hsync back porch
+	constant HRE: integer:= 112;	-- hsync retrace
+	constant LIN: integer:= 1024;	-- vertical lines
+	constant VFP: integer:= 1;		-- vsync front porch
+	constant VBP: integer:= 38;		-- vsync back porch
+	constant VRE: integer:= 3;		-- vsync retrace
+	constant offset: integer:= 256; -- center the display
+
+	-- counter variables
+	signal count_1688, count_1688_next: std_logic_vector(11 downto 0);
+	signal count_1066, count_1066_next: std_logic_vector(10 downto 0);
+    signal h_count: std_logic_vector(11 downto 0);
+    signal v_count: std_logic_vector(10 downto 0);
+
+	-- control variables
+	signal h_end, v_end, h_screen, v_screen: std_logic;
+    
+    -- output register sync signals
+	signal vsync_reg: std_logic;
+	signal hsync_reg: std_logic;
+
+	-- Additional varibles for project, doubling the horizontal length
+	signal sw0_reg, sw0_reg1: std_logic;
+	signal h_add: std_logic_vector(11 downto 0);
+    
+    -- Output colours vectors
+	signal output_colour: std_logic_vector(11 downto 0);
+
+	-- Data from memory ready to represent
+	signal data_to_vga: std_logic_vector(11 downto 0);
+	
+	-- Temperatura variables
+	signal t_temperature: std_logic_vector(10 downto 0);
+	signal temperature: std_logic_vector(10 downto 0);
+	signal alarm: std_logic;
+
+	begin
+    
+    -- register to output sync
+	process (clk, reset)
+		begin
+        if(clk'event and clk = '1') then
+            if (reset = '1') then	
+                -- All the signals are reseted in the processes below
+            else
+    			vsync <= vsync_reg;
+    			hsync <= hsync_reg;
+
+    			sw0_reg <= sw0;
+    			sw0_reg1 <= sw0_reg;
+
+                --Sync colours
+                red <= output_colour(11 downto 8);
+                green <= output_colour(7 downto 4);
+                blue <= output_colour(3 downto 0);
+
+            end if;
+        end if;
+		
+	end process;
+    
+	-- 1688 counter, clock times for horizontal pixels
+	counter1688: process (clk, reset)
+		begin
+        if (clk'event and clk = '1') then
+            if (reset = '1') then
+                count_1688_next <= (others => '0');
+            else
+                -- it resets when the line ends, if not it increases in 1
+                if (h_end = '1') then
+                    count_1688_next <= (others => '0');
+                else
+                    count_1688_next <= count_1688 + 1;
+                end if;
+            end if;
+        end if;
+	end process;
+
+    -- 1066 counter, 1688 counter for lines
+	counter1066: process (clk, reset)
+		begin
+        if (clk'event and clk = '1') then
+            if (reset = '1') then
+                count_1066_next <= (others => '0');
+            else
+                -- it resets when the frame ends, if not it increases in 1
+                if (v_end = '1') then
+                    count_1066_next <= (others => '0');
+                elsif (h_end = '1') then
+                    count_1066_next <= count_1066 + 1;
+                end if;
+            end if;
+        end if;
+	end process;
+    
+    -- VGA representation
+	signalgen: process (clk, reset)
+		begin
+        if(clk'event and clk = '1') then 
+			if (reset = '1' or v_end = '1')  then 
+                -- force the output colour to all '0' when the frame ends or the system resets
+				output_colour <= (others => '0');
+				addr_out <= (others => '0');
+				data_to_vga <= (others => '0');
+			else
+				-- detection of switch in order to get even indexes of addresses
+				if(sw0_reg1 = '0') then
+					h_add <= (others => '0');
+				else
+					h_add <= h_count + 2;
+				end if;
+				-- Read from memory
+				if(h_count < 1279 and h_count >= -1 and v_count < 512 and v_count >=0) then
+						addr_out <= h_count + 1 + h_add;
+						data_to_vga <= data_out;
+				end if;
+				-- inside the screen
+				if(v_screen = '1' and h_screen = '1') then
+					-- set output to '0' below the temperature display
+					if(v_count(8 downto 0) = trigger_level and h_count < 20 and v_count < 512 and v_count >=0) then
+						output_colour <= "000000001111";
+					else
+						-- Detect point in the detected signal
+						if(data_out(11 downto 3) = v_count(8 downto 0) and alarm = '0' and v_count < 512 and v_count >= 0) then
+							output_colour <= "111111110000";
+						else
+							-- Represent temperature bar
+							if(count_1066 >= (538 + VBP + offset) and count_1066 <= (573 + VBP + offset)) then
+								if(h_count = t_temperature) then
+									output_colour <= "000000001111" ;
+								elsif((count_1066 >= (541 + VBP + offset)) and (count_1066 <= (571 + VBP + offset)) ) then 
+									if(h_count <= temperature) then
+										if(alarm = '0') then
+											output_colour <= "000011110000";
+										else
+											output_colour <= "111100000000";
+										end if;
+									else
+										output_colour <= (others => '0');
+									end if;
+								else
+									output_colour <= (others => '0');
+								end if;
+							else
+								output_colour <= (others => '0');
+							end if;
+					    end if;
+					end if;
+				else
+					output_colour <= (others => '0');
+				end if;
+			end if;		
+        end if;
+	end process;
+
+    -- set hsync and vsync signals to '1' when any counter is on retrace
+	hsync_reg <= '1' when count_1688 < (PPL+HFP+HBP) else '0';
+	vsync_reg <= '1' when count_1066 < (LIN+VFP+VBP) else '0';
+	
+    -- set the counter values to the actual ones
+	count_1688 <= count_1688_next;
+	count_1066 <= count_1066_next;
+	
+    -- set these internal signals to '1' when a line or a frame is ended
+	h_end <= '1' when count_1688 = (PPL + HFP + HBP + HRE - 1) else '0';
+	v_end <= '1' when count_1066 = (LIN + VFP + VBP + VRE - 1) else '0';
+	
+    -- set internal flags to '1' when the counters are inside the active area
+	h_screen <= '1' when (count_1688 > HBP and count_1688 <= HBP+1280) else '0';
+	v_screen <= '1' when (count_1066 > VBP and count_1066 <= VBP+1024) else '0';
+
+	-- Substraction count to get a value refered to start of display, can be negative
+	v_count <= count_1066 - VBP - offset;
+	h_count <= count_1688 - HBP;
+	
+	-- Separate the GPIO signal in alarm, temperature and t_temperature
+	alarm <= gpio_in(22);
+	temperature <= gpio_in(21 downto 11);
+	t_temperature <= gpio_in(10 downto 0);
+
+end arch;
